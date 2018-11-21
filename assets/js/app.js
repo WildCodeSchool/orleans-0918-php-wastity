e65eac6237b061738912a111c6d777ee5115f859
tom <tom.guibard45@gmail.com>
2018-11-21 16:30:00 +0100

    Intégration de la barre de navigation / footer des entreprises

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,9 @@
 // any CSS you require will output into a single css file (app.css in this case)
 
 require('../css/app.scss');
-
+require('../css/projectVar.scss');
+require('../css/company.scss');
+require('@fortawesome/fontawesome-free/css/all.min.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 var $ = require('jquery');
