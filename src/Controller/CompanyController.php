2d5d65364944963efaa7d512fca81adafab76e1c
tom <tom.guibard45@gmail.com>
2018-11-21 18:16:47 +0100

    Correction des erreurs

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompanyController extends AbstractController
+{
+    /**
+     * @Route("/company", name="test")
+     */
+    public function index()
+    {
+        return $this->render('Company/index.html.twig');
+    }
+}
