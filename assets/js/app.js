7000bac3ddb2337acdf86aea9ab38ff5d2eecef4
Patricia Batjom <patriciabatjom@gmail.com>
2018-11-21 15:24:06 +0100

    création de la barre de navigation foodhero

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,15 @@
 // any CSS you require will output into a single css file (app.css in this case)
 
 require('../css/app.scss');
+require('../css/projectVar.scss');
+require('../css/foodHero.scss');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require("bootstrap");
 
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 var $ = require('jquery');
 
+
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
---
+++ src/Controller/FoodHeroController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class FoodHeroController extends AbstractController
+{
+    /**
+     * @Route("/foodHero", name="foodHero_index")
+     */
+    public function index():Response
+    {
+        return $this->render('foodHero/navBar.html.twig');
+    }
+
+}
