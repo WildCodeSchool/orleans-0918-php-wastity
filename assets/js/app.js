77f5480812f11982ded9185a51a6fca764206b53
Patricia Batjom <patriciabatjom@gmail.com>
2018-11-22 17:33:45 +0100

    création page catégories

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,10 @@
 // any CSS you require will output into a single css file (app.css in this case)
 
 require('../css/app.scss');
+require('../css/projectVar.scss');
+require('../css/profil.scss');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require("bootstrap");
 
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
