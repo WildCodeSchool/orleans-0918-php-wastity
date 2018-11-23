113e3c6680ba5fb1639cb9d9e6e370fe317427c1
Gallier Guillaume <guillaume.gallier@sfr.fr>
2018-11-23 09:07:51 +0100

    modification du app.js ajout chevron

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@
 // any CSS you require will output into a single css file (app.css in this case)
 
 require('../css/app.scss');
-
+require('../css/formAssoc.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 var $ = require('jquery');
