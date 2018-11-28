57d0f592aa96141551e2aee90b13829ae3b4b5f0
Vivien Piveteau <vivien.piveteau@hotmail.fr>
2018-11-28 16:30:48 +0100

    Fixtures for Association and Company

---
+++ src/DataFixtures/AssociationFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\Association;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AssociationFixtures extends Fixture
+{
+    private $streetAdress = [
+        'Rue Roussy',
+        'Boulevard Amiral Courbet',
+        'Avenue de Paris',
+        'Allée de la concorde',
+        'Boulevard de Chateaudun',
+        'Rue de la Bourie Blanche',
+        'Rue de la gare',
+        'Rue Emile Zola',
+        'Rue Marcel Proust',
+        'Boulevard Alexandre martin',
+    ];
+    
+    public function load(ObjectManager $manager)
+    {
+        for ($i=1; $i <=15; $i++) {
+            $association=new Association();
+            $faker  =  Faker\Factory::create('fr_FR');
+            $fakerUS = Faker\Factory::create('en_US');
+            $association->setName($fakerUS->company);
+            $association->setAdress(rand(1,100).' '.$this->streetAdress[array_rand($this->streetAdress)]);
+            $association->setPostalCode('45000');
+            $association->setTown('Orléans');
+            $association->setMail($faker->email);
+            $association->setPhone($faker->phoneNumber);
+            $manager->persist($association);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/CompanyFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Company;
+use Faker;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CompanyFixtures extends Fixture
+{
+    private $streetAdress = [
+        'Rue Jules Lemaitre',
+        'Boulevard Dupanloup',
+        'Avenue de Paris',
+        'Rue du bourdon blanc',
+        'Quai du chatelet',
+        'Rue du poirier',
+        'Rue de la gare',
+        'Rue Emile Zola',
+        'Rue Saint Flou',
+        'Boulevard Alexandre martin',
+    ];
+    
+    private $companyType = [
+      'boulangerie',
+      'épicerie',
+      'supermarché',
+      'restaurant',
+      'primeur',
+      'magasin'
+    ];
+    
+    public function load(ObjectManager $manager)
+    {
+        for ($i=1; $i <=15; $i++) {
+            $company=new Company();
+            $faker  =  Faker\Factory::create('fr_FR');
+            $fakerUS = Faker\Factory::create('en_US');
+            $company->setType($this->companyType[array_rand($this->companyType)]);
+            $company->setName($fakerUS->company);
+            $company->setAddress(rand(1,100).' '.$this->streetAdress[array_rand($this->streetAdress)]);
+            $company->setPostalCode('45000');
+            $company->setCity('Orléans');
+            $company->setEmail($faker->email);
+            $company->setPhone($faker->phoneNumber);
+            $manager->persist($company);
+        }
+
+        $manager->flush();
+    }
+}
