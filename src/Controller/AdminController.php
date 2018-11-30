28c53600e3d73a9d20709f15e7e21cfa3b25533e
Gallier Guillaume <guillaume.gallier@sfr.fr>
2018-11-30 11:33:54 +0100

    travis et security

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,12 +8,9 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
 
 class AdminController extends AbstractController
 {
