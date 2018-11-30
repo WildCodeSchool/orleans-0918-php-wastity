c2589e8fba18c93672ebc46c060936da33633656
Patricia Batjom <patriciabatjom@gmail.com>
2018-11-30 10:59:15 +0100

    rectification des versions de migration pour la création de la tablea schedule et day of week

--- src/Migrations/Version20181130093429.php
+++ src/Migrations/Version20181130093429.php
@@ -8,16 +8,17 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181128163529 extends AbstractMigration
+final class Version20181130093429 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE schedule ADD day_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE schedule (id INT AUTO_INCREMENT NOT NULL, opening_am INT DEFAULT NULL, closing_am INT DEFAULT NULL, opening_pm INT DEFAULT NULL, closing_pm INT DEFAULT NULL, company_id INT DEFAULT NULL, day_id INT DEFAULT NULL, INDEX IDX_5A3811FB979B1AD6 (company_id), INDEX IDX_5A3811FB9C24126 (day_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE days_of_week (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(45) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE schedule ADD CONSTRAINT FK_5A3811FB979B1AD6 FOREIGN KEY (company_id) REFERENCES company (id)');
         $this->addSql('ALTER TABLE schedule ADD CONSTRAINT FK_5A3811FB9C24126 FOREIGN KEY (day_id) REFERENCES days_of_week (id)');
-        $this->addSql('CREATE INDEX IDX_5A3811FB9C24126 ON schedule (day_id)');
     }
 
     public function down(Schema $schema) : void
@@ -25,8 +26,9 @@ final class Version20181128163529 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE schedule DROP FOREIGN KEY FK_5A3811FB979B1AD6');
         $this->addSql('ALTER TABLE schedule DROP FOREIGN KEY FK_5A3811FB9C24126');
-        $this->addSql('DROP INDEX IDX_5A3811FB9C24126 ON schedule');
-        $this->addSql('ALTER TABLE schedule DROP day_id');
+        $this->addSql('DROP TABLE schedule');
+        $this->addSql('DROP TABLE days_of_week');
     }
 }
