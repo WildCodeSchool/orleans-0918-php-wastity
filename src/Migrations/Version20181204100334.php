272ff1e133c2e438b4fb7666a4e7ec725f33ce4c
Vivien Piveteau <vivien.piveteau@hotmail.fr>
2018-12-04 16:03:42 +0100

    minor adjustments

--- src/Migrations/Version20181204100334.php
+++ src/Migrations/Version20181204100334.php
@@ -15,7 +15,7 @@ final class Version20181204100334 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE schedule CHANGE opening_am opening_am TIME NOT NULL, CHANGE closing_am closing_am TIME NOT NULL, CHANGE opening_pm opening_pm TIME NOT NULL, CHANGE closing_pm closing_pm TIME NOT NULL');
+        $this->addSql('ALTER TABLE schedule CHANGE opening_am opening_am TIME DEFAULT NULL, CHANGE closing_am closing_am TIME DEFAULT NULL, CHANGE opening_pm opening_pm TIME DEFAULT NULL, CHANGE closing_pm closing_pm TIME DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
