2b90ec82710ada2e4879ca3da66b4ea4722afbeb
Gallier Guillaume <guillaume.gallier@sfr.fr>
2018-12-05 17:58:57 +0100

    ajout d'une photo de profil food hero

--- src/Form/FoodHeroType.php
+++ src/Form/FoodHeroType.php
@@ -6,6 +6,7 @@ use App\Entity\FoodHero;
 use App\Entity\User;
 use function Sodium\add;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,6 +17,12 @@ class FoodHeroType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('profileImageFile', FileType::class, [
+                'label' => 'Votre Image de Profil',
+                'attr' => [
+                    'class' => false,
+                ]
+                ])
             ->add('phone', TelType::class, [
                 'label' => 'Téléphone',
                 'attr' => [
