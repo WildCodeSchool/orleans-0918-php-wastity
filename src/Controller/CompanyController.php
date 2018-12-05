ad49908056e90b054b2c68dc1c6f7944eba0de9c
Patricia Batjom <patriciabatjom@gmail.com>
2018-12-05 14:20:25 +0100

    création de la vue

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -59,7 +59,7 @@ class CompanyController extends AbstractController
      */
     public function show(Company $company): Response
     {
-        return $this->render('Visitor/Company/show.html.twig', ['company' => $company]);
+        return $this->render('Visitor/Company/show.html.twig', ['company' => $company, 'schedules'=>$company->getSchedules()]);
     }
 
     /**
