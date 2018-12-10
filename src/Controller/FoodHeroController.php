cd348cb1f4ecdac1762e2971bd898523c411bc8e
Guisharko <42614415+Guisharko@users.noreply.github.com>
2018-12-10 09:42:23 +0100

    Update FoodHeroController.php

--- src/Controller/FoodHeroController.php
+++ src/Controller/FoodHeroController.php
@@ -36,7 +36,6 @@ class FoodHeroController extends AbstractController
             $em->flush();
 
             return $this->redirectToRoute('foodhero_edit', ['id' => $foodHero->getId()]);
-
         }
 
         return $this->render('Visitor/FoodHero/new.html.twig', [
