75c0968e72234ff9e49534e2a902c36c204c67bf
Vivien Piveteau <vivien.piveteau@hotmail.fr>
2018-12-17 15:28:40 +0100

    security company list offers

---
+++ src/Security/Voter/CompanyVoter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Company;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class CompanyVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, ['EDIT', 'VIEW', 'LIST_OFFERS'])
+            && $subject instanceof Company;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        $company=$subject;
+        
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'LIST_OFFERS':
+                return $user === $company->getUser();
+                break;
+        }
+        
+        return false;
+    }
+}
