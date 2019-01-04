b32b1cc4a8eab80de7250bf6742566933c61fd70
tom <tom.guibard45@gmail.com>
2019-01-04 16:18:22 +0100

    Oui

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -18,6 +18,9 @@ class ProfileController extends AbstractController
      */
     public function index():Response
     {
+        if (in_array('ROLE_ADMIN', $this->getUser()->getRoles())) {
+            return $this->redirectToRoute('admin_index');
+        }
         return $this->render('defineProfile.html.twig');
     }
 }
