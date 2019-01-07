dce57eaadb42d5190aec988e035039a1d2395ae1
Gallier Guillaume <guillaume.gallier@sfr.fr>
2019-01-07 11:02:11 +0100

    modal enlevée plus helpers

--- src/Form/OfferType.php
+++ src/Form/OfferType.php
@@ -18,36 +18,38 @@ class OfferType extends AbstractType
     {
         $builder
             ->add('pictureFile', VichImageType::class, [
-                'required' => false,
                 'allow_delete' => false,
-                'label' => 'Image de votre produit',
+                'label' => 'Image de votre produit *',
                 'download_uri' => false,
                 'image_uri' => false,
             ])
             ->add('weight', NumberType::class, [
                 'scale' => 2,
-                'label' => 'Poids',
+                'label' => 'Poids *',
                 'attr' => [
                 'placeholder' => 'en kg']
             ])
             ->add('start', DateTimeType::class, [
-                'label' => 'Disponible à partir de',
+                'label' => 'Disponible à partir de *',
                 'widget' => 'single_text',
                 'invalid_message' => 'La date doit être 
                 de la forme AAAA-MM-JJ HH:MM',
+                'help' => 'ex:2019-01-31 20:30',
                 'attr' => [
-                    'placeholder' => '2018-12-31 20:30']
+                    'placeholder' => '2019-01-31 20:30']
             ])
             ->add('end', DateTimeType::class, [
-                'label' => 'Jusqu\'à',
+                'label' => 'Jusqu\'à *',
                 'widget' => 'single_text',
                 'invalid_message' => 'La date doit être 
                 de la forme AAAA-MM-JJ HH:MM',
+                'help' => 'ex:2019-12-31 20:30',
                 'attr' => [
-                    'placeholder' => '2018-12-31 20:30']
+                    'placeholder' => '2019-12-31 20:30']
             ])
             ->add('description', TextareaType::class, [
                 'label' => 'Description',
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'Décrivez votre produit. Ex : Baguettes, Boites de conserves, ...']
             ])
@@ -55,12 +57,14 @@ class OfferType extends AbstractType
                 'label' => 'Information complémentaire',
                 'required'=> false
             ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Offer::class,
+            'help' => '* champs obligatoires',
         ]);
     }
 }
