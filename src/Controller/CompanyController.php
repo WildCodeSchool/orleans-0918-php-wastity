f3fcb11d46fc76f7dfeced3376a95ebece5ecb60
Vivien Piveteau <vivien.piveteau@hotmail.fr>
2019-01-09 14:56:52 +0100

    correction

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -70,7 +70,7 @@ class CompanyController extends AbstractController
      * @param Company $company
      * @param OfferRepository $offerRepository
      * @return Response
-     * @IsGranted("view", subject="company")
+     * @IsGranted("companyView", subject="company")
      */
     public function listOffers(Company $company, Request $request, PaginatorInterface $paginator)
     {
@@ -110,7 +110,7 @@ class CompanyController extends AbstractController
      * @Route("/{id}/record", name="company_record", methods="GET")
      * @param Company $company
      * @return Response
-     * @IsGranted("view", subject="company")
+     * @IsGranted("companyView", subject="company")
      */
     public function record(Company $company): Response
     {
@@ -124,7 +124,7 @@ class CompanyController extends AbstractController
      * @Route("/{id}/showCompany", name="company_show", methods="GET|POST")
      * @param Company $company
      * @return Response
-     * @IsGranted("view", subject="company")
+     * @IsGranted("companyView", subject="company")
      */
     public function showCompany(Company $company, Request $request, UserRepository $userRepository): Response
     {
@@ -250,7 +250,7 @@ class CompanyController extends AbstractController
      * @Route("/{id}/statistics", name="company_show_statistics", methods="GET|POST")
      * @param company $company
      * @return Response
-     * @IsGranted("view", subject="company")
+     * @IsGranted("companyView", subject="company")
      */
     public function showStatistics(
         Company $company,
