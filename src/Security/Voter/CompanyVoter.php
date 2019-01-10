2821edcebfae585ac10bc11a93f2809e3361bbc5
tom <tom.guibard45@gmail.com>
2019-01-10 16:10:59 +0100

    Enlevage des voters

--- src/Security/Voter/CompanyVoter.php
+++ src/Security/Voter/CompanyVoter.php
@@ -6,19 +6,17 @@ use App\Entity\Company;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 class CompanyVoter extends Voter
 {
     const VIEW = 'companyView';
     const COMPANY_ADMIN = 'companyAdmin';
-    const COMPANY_MEMBER = 'companyMember';
 
     protected function supports($attribute, $subject)
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::VIEW, self::COMPANY_ADMIN, self::COMPANY_MEMBER])
+        return in_array($attribute, [self::VIEW, self::COMPANY_ADMIN])
             && $subject instanceof Company;
     }
 
