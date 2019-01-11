ce5adacbdd553fddbb021c229c5142a243639c0d
Lucile Varloteaux <varloteaux.lucile@gmail.com>
2019-01-11 10:35:17 +0100

    retrait de la liste des offres de la company les offres périmé et livré

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -69,12 +69,20 @@ class CompanyController extends AbstractController
      * @Route("/{id}/offers", name="company_show_offers", methods="GET")
      * @param Company $company
      * @param OfferRepository $offerRepository
+     * @param Request $request
+     * @param PaginatorInterface $paginator
      * @return Response
+     * @throws \Exception
      * @IsGranted("companyView", subject="company")
      */
-    public function listOffers(Company $company, Request $request, PaginatorInterface $paginator)
+    public function listOffers(
+        Company $company,
+        OfferRepository $offerRepository,
+        Request $request,
+        PaginatorInterface $paginator)
     {
-        $offers = $company->getOffers();
+        $offers = $offerRepository->findNotDeliveredForCompany(new \DateTime(), $company);
+//        $offers = $company->getOffers();
 
         $appointments = $paginator->paginate(
             $offers,
@@ -92,8 +100,9 @@ class CompanyController extends AbstractController
 
     /**
      * @Route("/{company}/oneOffer/{offer}", name="company_offer_card")
+     * @param Company $company
+     * @param Offer $offer
      * @return Response
-     * @throws \Exception
      */
     public function showOneOffer(
         Company $company,
